4499549efe5e4561e451bc500dcc8773c0c29689
Nenad Stojkovikj <nenadstojkovikj@gmail.com>
2014-07-10 11:50:48 +0200

    Modifing contact page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,11 +2,27 @@ require 'rails_helper'
 
 describe "pages" do
 
-  describe "Home" do
+  describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/pages/home'
       expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'contact'" do
+      visit '/pages/contact'
+      expect(page).to have_content('Contact')
+    end
+  end
+
+  describe "About Us page" do
+
+    it "should have the content 'About Us'" do
+      visit '/pages/about'
+      expect(page).to have_content('About Us')
     end
   end
 end
