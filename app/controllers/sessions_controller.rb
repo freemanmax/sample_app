f3802962a035752f10259efd6f3cb49f4ba7f571
Nenad Stojkovikj <nenadstojkovikj@gmail.com>
2014-07-21 03:12:53 +0200

    Login and logout system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      
+      log_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination'
       @title = 'Login'
@@ -13,6 +14,7 @@ class SessionsController < ApplicationController
     end
   end
   def destroy
-
+    log_out
+    redirect_to root_url
   end
 end
