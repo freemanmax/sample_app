9f7bf87b5d5aee474aeee62ada9db17bf36b27b7
Nenad Stojkovikj <nenadstojkovikj@gmail.com>
2014-07-22 17:41:14 +0200

    Added authentication and friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_action :loged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
      @user = User.find(params[:id])
      @title = @user.name
@@ -34,7 +36,16 @@ class UsersController < ApplicationController
   end
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    def loged_in_user
+      unless loged_in?
+         store_location
+         redirect_to login_url, notice: "Please sign in."
+      end 
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
