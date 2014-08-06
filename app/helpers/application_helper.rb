641d3c92a92708f48c80e5d32cadd39b31b97f5a
Nenad Stojkovikj <nenadstojkovikj@gmail.com>
2014-08-06 04:02:43 +0200

    Change style of the (Home page)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,24 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
-  # Image logo 
-  def logo
-    image_tag("rails.png", :alt => "Sample App", :class => "round")
+  def post_time(time)
+    t1 = time
+    t2 = Time.now+1
+    t3 = t2-t1
+    if t3 <= 60      
+      "#{t3.to_i.to_s}s"
+    elsif t3 > 60 and t3 <= 3600
+      t = t3/60.to_i
+      "#{t.to_i.to_s}m"
+    elsif t3 > 3600 and t3 <= 86400
+      t = t3/(60*60).to_i
+      "#{t.to_i.to_s}h"
+    else
+      if t1.year == t2.year
+        "#{t1.strftime('%-d %b')}"
+      else
+        "#{t1.strftime('%-d %b %Y')}"
+      end
+    end
   end
 end
